7e427111ea74bed5fd447e8236a7b13f3f7c9651
SzlomoGoldstein <gadulecki@gmail.com>
2024-03-26 20:32:23 +0100

    wylogowywanie

---
+++ DotNetMentor.PageMonitor.Application/Logic/User/LogoutCommand.cs
@@ -0,0 +1,54 @@
+﻿using DotNetMentor.PageMonitor.Application.Exceptions;
+using DotNetMentor.PageMonitor.Application.Interfaces;
+using DotNetMentor.PageMonitor.Application.Logic.Abstractions;
+using DotNetMentor.PageMonitor.Domain.Entities;
+using FluentValidation;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetMentor.PageMonitor.Application.Logic.User
+{
+    public static class LogoutCommand
+    {
+        public class Request : IRequest<Result> 
+        {
+
+        }
+
+        public class Result 
+        {
+        }
+
+        public class Handler : BaseCommandHandler, IRequestHandler<Request, Result> 
+        {
+            private readonly IPasswordManager _passwordManager;
+            public Handler(ICurrentAccountProvider currentAccountProvider, IApplicationDbContext applicationDbContext) :
+                base(currentAccountProvider, applicationDbContext)
+            {
+                
+            }
+
+            public async Task<Result> Handle(Request request, CancellationToken cancellationToken) 
+            {
+                return new Result
+                {
+
+                };
+            }
+
+
+            public class Validator : AbstractValidator<Request> 
+            {
+                public Validator() 
+                {
+
+                }
+            }
+        }
+    }
+}
--- DotNetMentor.PageMonitor.WebApi/Controllers/UserController.cs
+++ DotNetMentor.PageMonitor.WebApi/Controllers/UserController.cs
@@ -44,6 +44,14 @@ namespace DotNetMentor.PageMonitor.WebApi.Controllers
             return Ok(new JwtToken() { AccessToken = token });
         }
 
+        [HttpPost]
+        public async Task<ActionResult> Logout() 
+        {
+            var logoutResult = await _mediator.Send(new LogoutCommand.Request());
+            DeleteTokenCookie();
+            return Ok(logoutResult);
+        }
+
         private void SetTokenCookie(string token) 
         {
             var cookieOption = new CookieOptions()
@@ -66,6 +74,14 @@ namespace DotNetMentor.PageMonitor.WebApi.Controllers
             }
 
             Response.Cookies.Append(CookieSettings.CookieName, token, cookieOption);
+        }
+
+        private void DeleteTokenCookie() 
+        {
+            Response.Cookies.Delete(CookieSettings.CookieName, new CookieOptions() 
+            {
+                HttpOnly = true,
+            });
         }
     }
 }
